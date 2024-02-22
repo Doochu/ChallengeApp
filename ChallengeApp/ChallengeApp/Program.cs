e7aa2e59e73939c6078ea34f3b9f1f7bcc47b366
Doochu <arturdooszynski@gmail.com>
2024-02-23 00:16:39 +0100

    Praca domowa dz.4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// liczby równe ==
+// liczba mniejsza >
+// liczba większa <
+// liczba mniejsza lub równa  >=
+// liczba większa lub równa <=
+// liczba różna !=
+
+var name = "Artur";
+var sex = "mężczyzna";
+var age = 17;
+
+if (sex != "mężczyzna" && age < 30)
+{
+    Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("EWA, LAT 30");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("MĘŻCZYZNA NIEPEŁNOLETNI");
+}
